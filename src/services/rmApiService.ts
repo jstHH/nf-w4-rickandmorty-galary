3a483e28d8940c9662db4ffa5fa8589e2b6209f1
Joachim <joachim.stolle@hotmail.de>
2022-04-20 16:04:17 +0200

    changed fetch to axios

--- src/services/rmApiService.ts
+++ src/services/rmApiService.ts
@@ -1,8 +1,17 @@
 import axios from "axios";
 
+type rmApiServiceProps = {
+    characterID: number;
+}
 
 export default function fetchCharacters () {
     return axios.get('https://rickandmortyapi.com/api/character')
+        .then(response => response.data)
+        .catch(console.error)
+}
+
+export function fetchCharacter({characterID}: rmApiServiceProps) {
+    return axios.get(`https://rickandmortyapi.com/api/character/${characterID}`)
         .then(response => response.data)
         .catch(console.error)
 }
